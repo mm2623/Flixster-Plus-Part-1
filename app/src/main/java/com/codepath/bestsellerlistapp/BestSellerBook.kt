22e702dd1a0c5242f17cf930267d0032e9eb6f68
mm2623 <mm2623@njit.edu>
2023-02-11 22:23:04 -0500

    uploading to Flixster+

---
+++ app/src/main/java/com/codepath/bestsellerlistapp/BestSellerBook.kt
@@ -0,0 +1,34 @@
+package com.codepath.bestsellerlistapp
+
+import com.google.gson.annotations.SerializedName
+
+/**
+ * The Model for storing a single book from the NY Times API
+ *
+ * SerializedName tags MUST match the JSON response for the
+ * object to correctly parse with the gson library.
+ */
+class BestSellerBook {
+
+    @JvmField
+    @SerializedName("title")
+    var title: String? = null
+
+    //TODO description
+    @SerializedName("overview")
+    var description: String? = null
+
+    //TODO bookImageUrl
+    @SerializedName("poster_path") //poster_path
+    var imageUrl: String? = null
+
+//    @JvmField
+//    @SerializedName("author")
+//    var author: String? = null
+
+
+
+
+
+    //TODO-STRETCH-GOALS amazonUrl
+}
